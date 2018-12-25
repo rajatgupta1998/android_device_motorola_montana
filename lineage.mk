566cff9f5b3b04d14f7d8be452e14a15efefd91d
Rajat Gupta <rjtgupta84@gmail.com>
2018-12-25 22:32:08 +0530

    montana: vendor: CM > LineageOS
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -21,8 +21,8 @@ $(call inherit-product, device/motorola/montana/full_montana.mk)
 # Inherit base telephony stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
