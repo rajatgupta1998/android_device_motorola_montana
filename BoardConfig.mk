980f3c5d99ab73e813b2adf9d6bafe0a7c2063b9
Aymen Bousselham <aymenbousselham@gmail.com>
2018-12-28 19:28:08 +0530

    montana: Drop deprecated TARGET_KERNEL_APPEND_DTB
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,6 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := montana_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8937
-TARGET_KERNEL_APPEND_DTB := true
 
 # Audio
 #AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
