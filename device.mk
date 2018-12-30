2eb55d57a9063e938dbf83592711a2a8b0635246
Rajat Gupta <rjtgupta84@gmail.com>
2018-12-30 19:50:47 +0530

    montana: Build Vibrator HIDL
    Signed-off-by: Rajat Gupta <rjtgupta84@gmail.com>

--- device.mk
+++ device.mk
@@ -414,6 +414,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
      android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+     android.hardware.vibrator@1.0-impl
+
 # Visible Hardware Configs from moto
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vhw.xml:system/etc/vhw.xml \
